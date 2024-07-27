e0724cfe30e9949a1a49502c2631c3f9dbb6180e
tzh <3136374549@qq.com>
2024-07-27 16:57:12 +0800

    Initial commit

---
+++ src/main/kotlin/org/example/springsecuritykt/SpringSecurityKtApplication.kt
@@ -0,0 +1,11 @@
+package org.example.springsecuritykt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityKtApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/springsecuritykt/SpringSecurityKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.springsecuritykt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
